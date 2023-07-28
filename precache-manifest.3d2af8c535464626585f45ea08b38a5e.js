69a98a65b4c19ab774ad0b20ee6e72dc756605d9
gachibowlidiwakar <ksriyans@gmail.com>
2023-07-28 18:19:18 +0530

    Updates

---
+++ precache-manifest.3d2af8c535464626585f45ea08b38a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "194df82f49ecebd35d30",
+    "url": "/Web_Dev/static/css/main.f562a347.chunk.css"
+  },
+  {
+    "revision": "194df82f49ecebd35d30",
+    "url": "/Web_Dev/static/js/main.194df82f.chunk.js"
+  },
+  {
+    "revision": "be6f2844a0edce27491f",
+    "url": "/Web_Dev/static/js/1.be6f2844.chunk.js"
+  },
+  {
+    "revision": "fff05e5979660bc408c1",
+    "url": "/Web_Dev/static/js/runtime~main.fff05e59.js"
+  },
+  {
+    "revision": "dd2e21541517669e3d7f6f149740a7cf",
+    "url": "/Web_Dev/index.html"
+  }
+];
